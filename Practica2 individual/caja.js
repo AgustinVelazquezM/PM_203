10e59b6b34b5334fd1c64a77e31261e3cd213fbb
AgustinVelazquezM <ravm6895@gmail.com>
2026-05-19 20:46:05 -0600

    Caja.js update 2
    Se hace un nuevo commit 19/05/2026

--- Practica2 individual/caja.js
+++ Practica2 individual/caja.js
@@ -1,29 +1,103 @@
-
 let pedidos = [];
 
 let totalAcumulado = 0;
 
+const IVA = 0.16;
+
+const promociones = [
+
+    "Café Americano + Croissant por $75",
+
+    "2 Cappuccinos por $110",
+
+    "10% de descuento en bebidas calientes",
+
+    "Pay de Queso gratis en compras mayores a $250"
+
+];
+
+
 
 function agregarPedido(nombre, precio) {
 
+    const disponible =
+    descontarStock(nombre);
+
+
+
+    if(!disponible) {
+
+        alert("Producto agotado");
+
+        return;
+    }
+
+
+
     const pedido = {
+
         nombre: nombre,
+
         precio: precio
     };
 
+
+
     pedidos.push(pedido);
+
+
 
     totalAcumulado += precio;
 
-    actualizarPantalla();
-}
 
+
+    mostrarMenu();
+
+    actualizarPedidos();
+}
 
 function eliminarPedido(index) {
 
-    totalAcumulado -= pedidos[index].precio;
+    const pedidoEliminado =
+    pedidos[index];
+
+
+
+    regresarStock(
+        pedidoEliminado.nombre
+    );
+
+
+
+    totalAcumulado -=
+    pedidoEliminado.precio;
+
+
 
     pedidos.splice(index, 1);
 
-    actualizarPantalla();
+
+
+    mostrarMenu();
+
+    actualizarPedidos();
+}
+
+
+function calcularSubtotal() {
+
+    return totalAcumulado;
+}
+
+
+
+function calcularIVA(subtotal) {
+
+    return subtotal * IVA;
+}
+
+
+function calcularTotal(subtotal, iva) {
+
+    return subtotal + iva;
 }
